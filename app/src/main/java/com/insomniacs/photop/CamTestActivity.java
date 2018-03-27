09c30180a1c84409fdedb3c3f88c60f94b5578cd
Jitesh Vartak <jitesh.insomniacs@gmail.com>
2018-03-27 19:30:00 +0530

    stopCameraPreview

--- app/src/main/java/com/insomniacs/photop/CamTestActivity.java
+++ app/src/main/java/com/insomniacs/photop/CamTestActivity.java
@@ -194,7 +194,8 @@ public class CamTestActivity extends Activity {
         public void onPictureTaken(byte[] data, Camera camera) {
 
             new SaveImageTask().execute(data);
-            resetCam();
+            //resetCam();
+            stopCameraPreview();
             //Log.d(TAG, "onPictureTaken - jpeg");
         }
     };
